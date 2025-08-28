3e3e144962ef744d8abc724154083483eaf5f768
wesley van neck <wesleyvanneck550@gmail.com>
2025-08-28 15:30:23 +0200

    add basic source

---
+++ src/main/java/infiniteplayers/com/InfinitePlayers.java
@@ -0,0 +1,29 @@
+package com.infiniteplayers.infiniteplayers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InfinitePlayers extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("InfinitePlayers enabled!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("InfinitePlayers disabled!");
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        // If the server thinks it's full, always allow the player to join
+        if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
+            event.allow();
+        }
+    }
+}
